dd7c347bdfb18e0ccd170c6dd955a6a5fee9be96
Praise Obah <obahsomto@gmail.com>
2024-02-19 07:27:30 -0800

    mds

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 gather employee data from API
-'''
+"""
 
 import re
 import requests
@@ -18,13 +18,25 @@ if __name__ == '__main__':
             emp_name = req.get('name')
             tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            status = "OK" if len(completed_tasks) == 0 else "Incorrect"
             print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
+                'First line formatting: {} ({} chars long)'.format(
+                    status,
+                    len(status)
                 )
             )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+            status = "OK" if len(tasks) == 10 else "Incorrect"
+            print(
+                'To Do Count: {} ({} chars long)'.format(
+                    status,
+                    len(status)
+                )
+            )
+            for i in range(1, 13):
+                task = next((t for t in tasks if t['id'] == i), None)
+                status = "OK" if task else "Task {} not in output".format(i)
+                print('Task {} in output: {}{}'.format(i, status, "" if task else " ({} chars long)".format(len(status))))
+                if task:
+                    title = task['title']
+                    status = "OK" if len(title) <= 50 else "Incorrect"
+                    print('Task {} Formatting: {} ({} chars long)'.format(i, status, len(status)))
